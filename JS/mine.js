a0f739452e33c686cfd1e832d4c7ded7bc354fed
Alaahayek25 <alaalhayek25@gmail.com>
2020-08-26 22:42:02 -0700

    add js file for my web page

---
+++ JS/mine.js
@@ -0,0 +1,12 @@
+var name = prompt("what is your name!")
+alert("hello"+name)
+var team = prompt ("are you  a barcleona fan")  
+
+if (team === "yes" ) {
+    document.write('<h1> thanks to be with us ')
+}else {
+    alert('sorry it\'s for barcelona lover ')
+}
+
+console.log('name', name) ;
+console.log('team', team);
